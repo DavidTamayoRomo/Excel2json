f7ea318361a856ef2ef5b671bcd65cbbcd43c5fc
dtamayo <franklin.tamayo@quito.gob.ec>
2023-06-21 09:48:46 -0500

    carga masiva csv

--- src/main/java/gob/mdmq/mdmq_cmi_masivo/controller/datosController.java
+++ src/main/java/gob/mdmq/mdmq_cmi_masivo/controller/datosController.java
@@ -18,7 +18,10 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 import gob.mdmq.mdmq_cmi_masivo.model.datos;
 import gob.mdmq.mdmq_cmi_masivo.service.datosService;
@@ -28,8 +31,13 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.OfficeXmlFileException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -92,15 +100,15 @@ public class datosController {
                 if (currentBatch == batchSize) {
                     processBatch(currentBatch, datosFinales);
                     currentBatch = 0;
+                    datosFinales = new ArrayList<JsonObject>();
                 }
                 Row row = rows.next();
                 Iterator<Cell> cells = row.cellIterator();
 
                 int i = 0;
-                //Crear objeto Json
+                // Crear objeto Json
                 JsonObject jsonObject = new JsonObject();
-                
-                
+
                 while (cells.hasNext()) {
                     Cell cell = cells.next();
                     jsonObject.addProperty(datos1.get(i), cell.toString());
@@ -108,11 +116,12 @@ public class datosController {
                     i++;
                 }
 
-                //objeto2.put("datos", objeto);
+                // objeto2.put("datos", objeto);
                 datosFinales.add(jsonObject);
 
                 currentBatch++;
             }
+
             if (currentBatch > 0) {
                 processBatch(currentBatch, datosFinales);
             }
@@ -120,6 +129,66 @@ public class datosController {
         } catch (OfficeXmlFileException e) {
             e.printStackTrace();
             System.out.println("File type mismatch");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/uploadcsv")
+    public void uploadFileCSV(@RequestParam("file") MultipartFile file) {
+        try {
+            InputStream in = file.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+
+            /* CSVParser csvParser = new CSVParserBuilder()
+                .withSeparator(',').withIgnoreQuotations(false).build(); */ // Delimitador de coma
+            CSVParser csvParser = new CSVParserBuilder()
+                .withSeparator(';').withIgnoreQuotations(false).build(); // Delimitador de punto y coma
+
+            CSVReader csvReader = new CSVReaderBuilder(reader)
+                //.withSkipLines(1) // Omitir la primera línea si es un encabezado
+                .withCSVParser(csvParser)
+                .build();
+
+            //CSVReader csvReader = new CSVReader(reader);
+
+            List<String> headers = null;
+            List<JsonObject> datosFinales = new ArrayList<>();
+
+            int batchSize = 1000;
+            int currentBatch = 0;
+
+            String[] nextLine;
+            while ((nextLine = csvReader.readNext()) != null) {
+                if (currentBatch == batchSize) {
+                    processBatch(currentBatch, datosFinales);
+                    currentBatch = 0;
+                    datosFinales = new ArrayList<>();
+                }
+
+                if (headers == null) {
+                    headers = Arrays.asList(nextLine);
+                } else {
+                    JsonObject jsonObject = new JsonObject();
+                    for (int i = 0; i < headers.size(); i++) {
+                        String header = headers.get(i);
+                        String value = nextLine[i];
+                        jsonObject.addProperty(header, value);
+                        System.out.println("jsonObject: " + jsonObject);
+                    }
+                    datosFinales.add(jsonObject);
+                }
+
+                currentBatch++;
+            }
+
+            if (currentBatch > 0) {
+                processBatch(currentBatch, datosFinales);
+            }
+
+            csvReader.close();
+            reader.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -138,6 +207,7 @@ public class datosController {
                 }
 
             }
+            Thread.sleep(1000);
         } catch (Exception e) {
             e.printStackTrace();
         }
